122dbbfc2a94aa200e2451564a87fa83b00ea4a2
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-03-29 21:51:39 +0100

    Upgrade to rcgen 0.13

--- examples/generate-certs.rs
+++ examples/generate-certs.rs
@@ -1,20 +1,19 @@
 use std::fs;
 
-use rcgen::{BasicConstraints, Certificate, CertificateParams, IsCa};
+use rcgen::{BasicConstraints, CertificateParams, IsCa, KeyPair};
 
 fn main() -> Result<(), anyhow::Error> {
-    let mut ca_params = CertificateParams::new(vec!["localhost".to_owned()]);
+    let mut ca_params = CertificateParams::new(vec!["localhost".to_owned()])?;
     ca_params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
-    let ca_cert = Certificate::from_params(ca_params)?;
-    let ca_cert_pem = ca_cert.serialize_pem()?;
-    fs::write("tests/ca-certificate.pem", ca_cert_pem)?;
+    let ca_key = KeyPair::generate()?;
+    let ca_cert = ca_params.self_signed(&ca_key)?;
+    fs::write("tests/ca-certificate.pem", ca_cert.pem())?;
 
-    let ee_params = CertificateParams::new(vec!["localhost".to_owned()]);
-    let ee_cert = Certificate::from_params(ee_params)?;
-    let ee_cert_pem = ee_cert.serialize_pem_with_signer(&ca_cert)?;
-    fs::write("tests/ee-certificate.pem", ee_cert_pem)?;
-    let ee_key_pem = ee_cert.serialize_private_key_pem();
-    fs::write("tests/ee-key.pem", ee_key_pem)?;
+    let ee_params = CertificateParams::new(vec!["localhost".to_owned()])?;
+    let ee_key = KeyPair::generate()?;
+    let ee_cert = ee_params.signed_by(&ee_key, &ca_cert, &ca_key)?;
+    fs::write("tests/ee-certificate.pem", ee_cert.pem())?;
+    fs::write("tests/ee-key.pem", ee_key.serialize_pem())?;
 
     Ok(())
 }
