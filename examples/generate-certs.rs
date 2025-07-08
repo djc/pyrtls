f333d06ea9e36473af247f65dd5ae3a7558f98a2
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-07-08 11:10:14 +0200

    Upgrade rcgen to 0.14

--- examples/generate-certs.rs
+++ examples/generate-certs.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use rcgen::{BasicConstraints, CertificateParams, IsCa, KeyPair};
+use rcgen::{BasicConstraints, CertificateParams, IsCa, Issuer, KeyPair};
 
 fn main() -> Result<(), anyhow::Error> {
     let mut ca_params = CertificateParams::new(vec!["localhost".to_owned()])?;
@@ -8,10 +8,11 @@ fn main() -> Result<(), anyhow::Error> {
     let ca_key = KeyPair::generate()?;
     let ca_cert = ca_params.self_signed(&ca_key)?;
     fs::write("tests/ca-certificate.pem", ca_cert.pem())?;
+    let ca = Issuer::new(ca_params, ca_key);
 
     let ee_params = CertificateParams::new(vec!["localhost".to_owned()])?;
     let ee_key = KeyPair::generate()?;
-    let ee_cert = ee_params.signed_by(&ee_key, &ca_cert, &ca_key)?;
+    let ee_cert = ee_params.signed_by(&ee_key, &ca)?;
     fs::write("tests/ee-certificate.pem", ee_cert.pem())?;
     fs::write("tests/ee-key.pem", ee_key.serialize_pem())?;
 
